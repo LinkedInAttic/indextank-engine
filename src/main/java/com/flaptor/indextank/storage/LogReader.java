e7024360a7bde1381f2e70dfcb544feffbbd129c
Santiago Perez <santip@santip.com.ar>
2012-01-04 21:02:01 -0300

    Fixed a bug when recovering index with single sorted segments

--- src/main/java/com/flaptor/indextank/storage/LogReader.java
+++ src/main/java/com/flaptor/indextank/storage/LogReader.java
@@ -202,13 +202,14 @@ public class LogReader {
                             nextIndexSegment = s;
                             break;
                         }
-                        if (s.timestamp == token.get_timestamp()) {
+                        if (s.timestamp == segment.timestamp) {
                             foundCurrent = true;
                         }
                     }
                     if (!foundCurrent) {
                         // the file we just read to build the page has dissapeared. we can't
                         // guarantee that the next one didn't dissappear as well. ABORT!!!
+                        logger.error("Unable to find sorted segment for timestamp %s", token.get_timestamp());
                         return revertFromScratch();
                     }
                     if (nextIndexSegment != null) {
